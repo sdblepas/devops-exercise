5e391c52f2a52430065c4f8d029ede627bbf207d
Tal Barnoy <tal@barnoys.co.il>
2022-02-16 11:03:38 +0200

    test_app fix

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,16 @@
 import pytest
 import app
-
+import json
 @pytest.fixture
 def app():
     client = app.app.test_client
     yield client
 
 def test_signup(client):
-    response = client.post("/signup", data ={
+    response = client.post("/signup", data =json.dumps({
         "name":"Test"
         "password":"test"
-        })
+        }))
     assert response.status_code == 200
 def test_login():
     return True
